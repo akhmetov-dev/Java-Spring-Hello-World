43f6e562235a522223b9828ce6d94a2a1f1e47d3
Nikita <nikita-ahmetoff@yandex.ru>
2021-04-25 19:12:20 +0300

    function that mapped to <url>:8080/greeting added. this function return JSON-object, which fields is: - id; - greeting.

--- src/main/java/ru/ahmetoff/javaspringhelloworld/JavaSpringHelloWorldApplication.java
+++ src/main/java/ru/ahmetoff/javaspringhelloworld/JavaSpringHelloWorldApplication.java
@@ -6,16 +6,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @SpringBootApplication
 @RestController
 public class JavaSpringHelloWorldApplication {
+    private final AtomicLong counter = new AtomicLong();
 
     public static void main(String[] args) {
         SpringApplication.run(JavaSpringHelloWorldApplication.class, args);
     }
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "world") String name) {
-        return "Hello, " + name + "!";
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "world") String name) {
+        return new Greeting(counter.incrementAndGet(), "Hello, " + name + "!");
     }
 }
