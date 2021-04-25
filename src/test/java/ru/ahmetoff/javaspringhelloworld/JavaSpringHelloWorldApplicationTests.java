ad64597dc42708f7dc1868243339ee935460dc08
Nikita <nikita-ahmetoff@yandex.ru>
2021-04-25 18:51:11 +0300

    initial commit

---
+++ src/test/java/ru/ahmetoff/javaspringhelloworld/JavaSpringHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ahmetoff.javaspringhelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringHelloWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
